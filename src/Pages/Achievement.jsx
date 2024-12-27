5ebf83dec41a2f4bd052168d9497664d6b6c0531
amitlandge <amitlandge333@gmail.com>
2024-12-27 20:14:16 +0530

    add Achievement Section

---
+++ src/Pages/Achievement.jsx
@@ -0,0 +1,89 @@
+import { Box, Typography, Grid as Grid2 } from "@mui/material";
+
+const stats = [
+  { number: "50+", label: "Github Repository" },
+  { number: "6+", label: "Achievement" },
+  { number: "1+", label: "Experience" },
+  { number: "5+", label: "Certification" },
+];
+
+const Achievement = () => {
+  return (
+    <Box
+      sx={{
+        backgroundColor: "#1E1D1D",
+        color: "#fff",
+        padding: "4rem",
+      }}
+    >
+      <Grid2 container spacing={4} alignItems="center">
+        {/* Left Side */}
+        <Grid2 xs={12} md={4}>
+          <Box textAlign="center">
+            <Typography
+              variant="h1"
+              sx={{
+                color: "#FF0000",
+                fontWeight: "bold",
+                fontSize: "8rem",
+                marginBottom: "1rem",
+              }}
+            >
+              40+
+            </Typography>
+            <Typography
+              variant="h6"
+              sx={{
+                fontSize: "2rem",
+                fontWeight: "300",
+              }}
+            >
+              Completed Projects
+            </Typography>
+          </Box>
+        </Grid2>
+
+        {/* Right Side */}
+        <Grid2 xs={12} md={8}>
+          <Grid2 container spacing={4}>
+            {stats.map((stat, index) => (
+              <Grid2 item xs={12} sm={6} key={index}>
+                <Box
+                  sx={{
+                    backgroundColor: "#272424",
+                    padding: "3rem 1rem",
+                    textAlign: "center",
+                    boxShadow: "0px 4px 10px rgba(0,0,0,0.2)",
+                  }}
+                >
+                  <Typography
+                    variant="h3"
+                    sx={{
+                      color: "#FF0000",
+                      fontWeight: "bold",
+                      fontSize: "4rem",
+                      marginBottom: "1rem",
+                    }}
+                  >
+                    {stat.number}
+                  </Typography>
+                  <Typography
+                    variant="body1"
+                    sx={{
+                      fontSize: "1.2rem",
+                      fontWeight: "300",
+                    }}
+                  >
+                    {stat.label}
+                  </Typography>
+                </Box>
+              </Grid2>
+            ))}
+          </Grid2>
+        </Grid2>
+      </Grid2>
+    </Box>
+  );
+};
+
+export default Achievement;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import { Box } from "@mui/material";
 import Hero from "./Hero";
 import Cards from "./Cards";
+import Achievement from "./Achievement";
 
 const Home = () => {
   return (
     <Box>
       <Hero />
       <Cards />
+      <Achievement />
     </Box>
   );
 };
